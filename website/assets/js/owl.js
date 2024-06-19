3b2f9494a90c3450e8973997b8ccfa7c06964ec8
devil1315 <rohit131511@authenticode.in>
2024-06-19 18:50:49 +0530

    EOD 19-06-2024

--- website/assets/js/owl.js
+++ website/assets/js/owl.js
@@ -148,6 +148,30 @@ $(".premium-carousel").owlCarousel({
     },
   },
 });
+$(".account-store-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  navText: [
+    "<i class='fa fa-chevron-left'></i>",
+    "<i class='fa fa-chevron-right'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 1,
+      nav: true,
+      loop: true,
+    },
+  },
+});
 
 // thumbnail
 $(document).ready(function () {
